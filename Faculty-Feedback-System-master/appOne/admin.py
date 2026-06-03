5a24e0232fae4df0ae88557324de4ff3ef1036f0
unknown <155887367+rishideka@users.noreply.github.com>
2026-06-03 13:43:33 +0530

    Delete Faculty-Feedback-System-master directory

--- Faculty-Feedback-System-master/appOne/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from appOne.models import Feedback, UserProfile, TeacherProfile, Subject
-
-admin.site.register(Feedback)
-admin.site.register(UserProfile)
-admin.site.register(TeacherProfile)
-admin.site.register(Subject)
